84c9f7af3dcde3f7dcfd8c6190237e9173bb3aae
Felix8665 <92393635+Felix8665@users.noreply.github.com>
2023-03-16 15:59:18 -0400

    plot signal et FFT fonctionnel

--- DFT.py
+++ DFT.py
@@ -3,25 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 def fct_DFT(signal,sample_rate):
 
-    # Appliquer une fenêtre de Hamming pour meilleur DFT
-    window = np.hamming(len(signal))
-    windowed_signal = signal * window
+    # Calculer la DFT
+    fft_signal = np.fft.fftshift(np.fft.fft(signal))
 
-    # Calculer la DFT de la série temporelle fenêtrée
-    dft = np.fft.fft(windowed_signal)
-
+    # Appliquer une fenêtre de Hamming
+    window = np.hamming(len(fft_signal))
+    windowed_signal = fft_signal * window
 
     # Trouver les fréquences et amplitudes des harmoniques
     freqs = np.fft.fftfreq(len(signal), 1/sample_rate)
     harmonic_indices = np.where(freqs > 0)
     harmonic_freqs = freqs[harmonic_indices]
-    harmonic_amplitude = np.abs(dft[harmonic_indices])
+    harmonic_amplitude = np.abs(fft_signal[harmonic_indices])
 
     # La phase des harmoniques
-    harmonic_phases = np.angle(dft[harmonic_indices])
+    harmonic_phases = np.angle(fft_signal[harmonic_indices])
 
-    # Estimer la fréquence fondamentale
+    # Trouver fréquence fondamentale
     fundamental_index = np.argmax(harmonic_amplitude)
     freq_fundamental = harmonic_freqs[fundamental_index]
 
-    return freq_fundamental, harmonic_amplitude, harmonic_phases
+    return freq_fundamental, harmonic_amplitude, harmonic_phases, fft_signal,freqs
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DFT import *
 
-with wave.open('note_basson_plus_sinus_1000_Hz.wav', 'rb') as wav_file:
+with wave.open('note_guitare_LAd.wav', 'rb') as wav_file:
     # nb d'échantillon du fichier
     num_frames = wav_file.getnframes()
 
@@ -13,39 +13,37 @@ with wave.open('note_basson_plus_sinus_1000_Hz.wav', 'rb') as wav_file:
     # lit tout le contenu du fichier et stock les amplitudes dans frames
     frames = wav_file.readframes(num_frames)
 
-    # Convertir les échantillons audio en une série temporelle de valeurs d'amplitude
+    #Convertir les échantillons audio en une série temporelle de valeurs d'amplitude
     signal = np.frombuffer(frames, dtype=np.int16)
 
+    freq_fundamental, harmonic_amplitude, harmonic_phases, fft_signal,freqs = fct_DFT(signal,sample_rate)
+    # graphique du FFT du signal
+    # x = np.arange(num_frames)
+    # plt.plot(x,signal)
+    # plt.title('Signal audio originale')
+    # plt.xlabel('Fréquence')
+    # plt.ylabel('Amplitude')
+    #
+    # plt.show()
+    #
+    # #graphique du FFT du signal
+    # x = np.arange(-(num_frames/2),num_frames/2,1)
+    # #plt.plot(x, np.abs(fft_signal))
+    # plt.plot(np.abs(fft_signal))
+    # plt.title('FFT du signal audio')
+    # plt.xlim(65000, 95000)
+    # plt.xlabel('Fréquence')
+    # plt.ylabel('Amplitude')
+    # plt.show()
 
-    freq_fundamental, harmonic_amplitude, harmonic_phases = fct_DFT(signal,sample_rate)
-
-    # Pad the harmonic amplitude and phases arrays with zeros if necessary
-    if len(harmonic_amplitude) < len(signal):
-        harmonic_amplitude = np.pad(harmonic_amplitude, (0, len(signal) - len(harmonic_amplitude)))
-    if len(harmonic_phases) < len(signal):
-        harmonic_phases = np.pad(harmonic_phases, (0, len(signal) - len(harmonic_phases)))
-
-    N = len(signal)  # length of the signal
-    n1 = np.arange(0, N, 1)  # create an array (start, stop, step)
-    t_fft1 = np.linspace(-0.5, 0.5, N, endpoint=False)
-
-    fig, axs = plt.subplots(3, 1)
-
-    # plot the original signal
-    axs[0].stem(n1, signal)
-    axs[0].set_title('x1')
-
-    # plot the harmonic amplitude
-    axs[1].stem(t_fft1, harmonic_amplitude)
-    axs[1].set_title('harmonic_amplitude')
-
-    # plot the harmonic phases
-    axs[2].stem(t_fft1, harmonic_phases)
-    axs[2].set_title('harmonic_phases')
+    # fq= ((freqs / 2)-1)
+    # plt.stem(fq, harmonic_amplitude)
+    # plt.xlabel('Fréquence (Hz)')
+    # plt.ylabel('Amplitude')
+    # plt.title('Amplitude des harmoniques')
+    # plt.show()
 
     plt.tight_layout()
     plt.show()
-
-
 
 
