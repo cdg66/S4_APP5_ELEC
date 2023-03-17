fff19d67421a89341b8dbb5484e79710b2c7cfc0
Felix8665 <92393635+Felix8665@users.noreply.github.com>
2023-03-17 16:20:55 -0400

    mise a jour amplitude et phase harmonique

--- DFT.py
+++ DFT.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 
 def fct_DFT(signal, sample_rate):
-    # Calculer la DFT
-    fft_signal = np.fft.fftshift(np.fft.fft(signal))
-
     # Appliquer une fenêtre de Hamming
-    window = np.hamming(len(fft_signal))
-    windowed_signal = fft_signal * window
+    window = np.hamming(len(signal))
+    windowed_signal = signal * window
+
+    # Calculer la DFT
+    fft_signal = np.fft.fftshift(np.fft.fft(windowed_signal))
+
+
 
     # Trouver la fondamentale
     autocorr = np.correlate(signal, signal, mode='full')
@@ -17,7 +19,7 @@ def fct_DFT(signal, sample_rate):
 
     # Convertir l'indice de la fréquence fondamentale en Hz
     freq_fundamental = sample_rate / freq_fundamental_index
-    fundamental_amplitude = np.abs(windowed_signal[freq_fundamental_index]) / len(windowed_signal) * 2
+    fundamental_amplitude = np.abs(fft_signal[freq_fundamental_index]) / len(windowed_signal) * 2
 
     # Trouver les indices des harmoniques
     n_harmonics = 32
@@ -28,10 +30,11 @@ def fct_DFT(signal, sample_rate):
         harmonic_indices.append(harmonic_index)
 
     # Calculer l'amplitude, la phase et la fréquence de chaque harmonique
-    harmonic_amplitude = np.abs(windowed_signal[harmonic_indices]) / len(windowed_signal) * 2
-    harmonic_phases = np.angle(windowed_signal[harmonic_indices])
+    harmonic_amplitude = np.abs(fft_signal[harmonic_indices]) / len(fft_signal) * 2
+    harmonic_phases = np.angle(fft_signal[harmonic_indices])
     #harmonic_frequencies = harmonic_indices / len(windowed_signal) * sample_rate
-    harmonic_frequencies = [index / len(windowed_signal) * sample_rate for index in harmonic_indices]
+    harmonic_frequencies = [index / len(fft_signal) * sample_rate for index in harmonic_indices]
+    harmonic_frequencies = [index / len(fft_signal) * sample_rate for index in harmonic_indices]
 
     # Retourner les résultats
     return freq_fundamental, fundamental_amplitude, harmonic_amplitude, harmonic_phases, harmonic_frequencies, fft_signal
